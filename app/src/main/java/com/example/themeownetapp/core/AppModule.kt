b4a39c16d07e82786f31160eeb57b5a6162e77d3
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 10:24:38 +0000

    Added AppModule for DI Added okhttp dependency Added interceptor for okhttp to see the full API call

---
+++ app/src/main/java/com/example/themeownetapp/core/AppModule.kt
@@ -0,0 +1,34 @@
+package com.example.themeownetapp.core
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    //Used to log responses from Retrofit, here we see the body of the response
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    /*
+    Here we create a network client and add the interceptor
+    I like to see the API calls with body on Logcat to see what's going on
+    */
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+}

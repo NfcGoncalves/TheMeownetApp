c871fbf81b5e11c4a059e6671b8bb4ccb91bb71d
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 12:45:09 +0000

    Added a listener for the buttons in the bottom navigation bar and set them to change the query

--- app/src/main/java/com/example/themeownetapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/themeownetapp/presentation/activities/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.themeownetapp.presentation.activities
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.themeownetapp.R
+import com.example.themeownetapp.core.Constants
 import com.example.themeownetapp.databinding.ActivityMainBinding
 import com.example.themeownetapp.presentation.adapters.CatImageAdapter
 import com.example.themeownetapp.presentation.viewmodels.CatGalleryViewModel
@@ -27,6 +29,16 @@ class MainActivity : AppCompatActivity() {
         //here we observe the livecycle data from the images
         viewModel.images.observe(this) {
             adapter.submitData(this.lifecycle, it)
+        }
+
+        //Set up the bottom navigation so that it changes the query when pressed
+        binding.fragmentGalleryBottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.imagesFragment -> viewModel.changeImageType(Constants.IMAGE_TYPE_IMAGE)
+                R.id.gifsFragment -> viewModel.changeImageType(Constants.IMAGE_TYPE_GIF)
+                R.id.bothFragment -> viewModel.changeImageType(Constants.IMAGE_TYPE_BOTH)
+            }
+            true
         }
     }
 }

44f1d0d65b80f375ed98da2d0c34d5152c958847
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 12:27:03 +0000

    Added Glide dependency to load images from url Added logic to load images on to the adapter Created an error vector when the images don't load

--- app/src/main/java/com/example/themeownetapp/presentation/adapters/CatImageAdapter.kt
+++ app/src/main/java/com/example/themeownetapp/presentation/adapters/CatImageAdapter.kt
@@ -5,17 +5,13 @@ import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.themeownetapp.R
 import com.example.themeownetapp.data.response.CatImage
 import com.example.themeownetapp.databinding.ItemCardCatBinding
 
 class CatImageAdapter :
     PagingDataAdapter<CatImage, CatImageAdapter.PhotoViewHolder>(DIFF_UTIL) {
-
-    inner class PhotoViewHolder(private val binding: ItemCardCatBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
         val binding =
@@ -25,7 +21,27 @@ class CatImageAdapter :
     }
 
     override fun onBindViewHolder(viewHolder: PhotoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = getItem(position)
+
+        if (currentItem != null) {
+            viewHolder.bind(currentItem)
+        }
+    }
+
+    inner class PhotoViewHolder(private val binding: ItemCardCatBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        //We load and bind the image here with Glide
+        //according to convention, bind function is created inside the viewholder
+        fun bind(photo: CatImage) {
+            binding.apply {
+                Glide.with(itemView)
+                    .load(photo.url)
+                    .centerCrop()
+                    .error(R.drawable.ic_broken_image)
+                    .into(binding.imageViewCat)
+            }
+        }
     }
 
     //Diffutil, a standard in an adapter

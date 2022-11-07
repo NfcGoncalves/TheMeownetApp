d45ae0a27db55b69cde498e95ed8f8a0494b0f4c
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 12:58:02 +0000

    Added some quick fixes Black text on navigation bar Changed the theme colour Added result caching

--- app/src/main/java/com/example/themeownetapp/presentation/viewmodels/CatGalleryViewModel.kt
+++ app/src/main/java/com/example/themeownetapp/presentation/viewmodels/CatGalleryViewModel.kt
@@ -3,6 +3,8 @@ package com.example.themeownetapp.presentation.viewmodels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.example.themeownetapp.core.Constants.IMAGE_TYPE_BOTH
 import com.example.themeownetapp.domain.repositories.CatRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,8 +18,9 @@ class CatGalleryViewModel @Inject constructor(private val repository: CatReposit
     private val currentQuery = MutableLiveData(IMAGE_TYPE_BOTH)
 
     //SwitchMap will helps us change the value of the query
+    //app was crashing when I was rotating the phone, so I'm caching the result
     val images = currentQuery.switchMap { queryString ->
-        repository.getCatImages(queryString)
+        repository.getCatImages(queryString).cachedIn(viewModelScope)
     }
 
     //Here we can change the image type by changing the query

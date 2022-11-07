96a0f1862b532ad6bbbb7bf34f4a20e8e401171d
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 09:59:47 +0000

    Tested the request on Postman Added the model with the parameters I will need int he future

--- app/src/main/java/com/example/themeownetapp/data/response/CatImage.kt
+++ app/src/main/java/com/example/themeownetapp/data/response/CatImage.kt
@@ -1,5 +1,12 @@
 package com.example.themeownetapp.data.response
 
 data class CatImage(
+    val id: String,
+    val url: String,
+    val categories: Category
+)
 
+data class Category(
+    val id: Int,
+    val name: String
 )

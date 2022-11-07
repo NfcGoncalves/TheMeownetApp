80f6ba15a17cd481a6f9c36beb30653a7d462911
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 10:29:02 +0000

    Added Retrofit provider to build the api Added the cat api provider, so we can call it from other classes

--- app/src/main/java/com/example/themeownetapp/core/AppModule.kt
+++ app/src/main/java/com/example/themeownetapp/core/AppModule.kt
@@ -1,11 +1,15 @@
 package com.example.themeownetapp.core
 
+import com.example.themeownetapp.BuildConfig
+import com.example.themeownetapp.data.request.CatRequestApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -31,4 +35,18 @@ object AppModule {
             .Builder()
             .addInterceptor(httpLoggingInterceptor)
             .build()
+
+    //Here we build our api to be provided
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BuildConfig.API_URL)
+        .client(okHttpClient)
+        .build()
+
+    //here is our Cat Api provider
+    @Singleton
+    @Provides
+    fun provideCatApi(retrofit: Retrofit): CatRequestApi = retrofit.create(CatRequestApi::class.java)
 }

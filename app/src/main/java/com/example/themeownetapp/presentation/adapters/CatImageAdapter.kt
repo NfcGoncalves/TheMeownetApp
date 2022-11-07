9da910a0676732e7f166d0cb0fa46b6c36611ed6
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 12:21:40 +0000

    Added onCreateViewHolder binding Added DiffUtil

--- app/src/main/java/com/example/themeownetapp/presentation/adapters/CatImageAdapter.kt
+++ app/src/main/java/com/example/themeownetapp/presentation/adapters/CatImageAdapter.kt
@@ -1,14 +1,15 @@
 package com.example.themeownetapp.presentation.adapters
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.themeownetapp.data.response.CatImage
 import com.example.themeownetapp.databinding.ItemCardCatBinding
 
-//TODO add diffUtil
 class CatImageAdapter :
-    PagingDataAdapter<CatImage, CatImageAdapter.PhotoViewHolder>() {
+    PagingDataAdapter<CatImage, CatImageAdapter.PhotoViewHolder>(DIFF_UTIL) {
 
     inner class PhotoViewHolder(private val binding: ItemCardCatBinding) :
         RecyclerView.ViewHolder(binding.root) {
@@ -16,11 +17,25 @@ class CatImageAdapter :
 
     }
 
-    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        val binding =
+            ItemCardCatBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return PhotoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(viewHolder: PhotoViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
-        TODO("Not yet implemented")
+    //Diffutil, a standard in an adapter
+    companion object {
+        private val DIFF_UTIL = object : DiffUtil.ItemCallback<CatImage>() {
+            override fun areItemsTheSame(oldItem: CatImage, newItem: CatImage) =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: CatImage, newItem: CatImage) =
+                oldItem == newItem
+        }
     }
 }

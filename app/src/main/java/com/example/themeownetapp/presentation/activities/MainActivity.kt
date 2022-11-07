84b4c48bf6ed768f15aba2d7f9f6d46af6f16baf
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 12:31:02 +0000

    Calling adapter from Activity

--- app/src/main/java/com/example/themeownetapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/themeownetapp/presentation/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.themeownetapp.databinding.ActivityMainBinding
+import com.example.themeownetapp.presentation.adapters.CatImageAdapter
 import com.example.themeownetapp.presentation.viewmodels.CatGalleryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,15 +12,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<CatGalleryViewModel>()
+    private val adapter = CatImageAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter = adapter
+        }
+
         //here we observe the livecycle data from the images
         viewModel.images.observe(this) {
-
+            adapter.submitData(this.lifecycle, it)
         }
     }
 }

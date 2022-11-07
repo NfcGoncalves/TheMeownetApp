18bd3b30db33108befb64c7440e1a38ae4a51d43
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 11:50:54 +0000

    Added Pager logic on to the Repository Added Constants related to pager

--- app/src/main/java/com/example/themeownetapp/core/Constants.kt
+++ app/src/main/java/com/example/themeownetapp/core/Constants.kt
@@ -4,4 +4,6 @@ object Constants {
 
     //Paging
     const val STARTING_PAGE_INDEX = 1
+    const val PAGE_SIZE = 30
+    const val MAX_SIZE = 100
 }
--- app/src/main/java/com/example/themeownetapp/domain/repositories/CatRepository.kt
+++ app/src/main/java/com/example/themeownetapp/domain/repositories/CatRepository.kt
@@ -1,6 +1,12 @@
 package com.example.themeownetapp.domain.repositories
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
+import com.example.themeownetapp.core.Constants.MAX_SIZE
+import com.example.themeownetapp.core.Constants.PAGE_SIZE
 import com.example.themeownetapp.data.request.CatRequestApi
+import com.example.themeownetapp.domain.paging.CatPagingSource
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -9,5 +15,19 @@ import javax.inject.Singleton
 class CatRepository @Inject constructor(
     private val catImageApi: CatRequestApi
 ) {
+
+    //This will then be called by the ViewModel
+    //it will return a pager from the paging library
+    fun getCatImages(
+        mimeTypes: String
+    ) = Pager(
+        config = PagingConfig(
+            //A page size of 30 and a max size of 100 looks good to make a demonstration
+            pageSize = PAGE_SIZE,
+            maxSize = MAX_SIZE,
+            enablePlaceholders = false
+        ),
+        pagingSourceFactory = { CatPagingSource(catImageApi, mimeTypes) }
+    ).liveData
 
 }

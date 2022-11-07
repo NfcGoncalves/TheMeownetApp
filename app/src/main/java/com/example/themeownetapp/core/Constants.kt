2c14495b393ea11c84d2ef0359e577bff89cb9d9
Nelson <nelson.goncalves@safetonet.com>
2022-11-07 13:20:33 +0000

    Made small fix to correct behaviour of the app

--- app/src/main/java/com/example/themeownetapp/core/Constants.kt
+++ app/src/main/java/com/example/themeownetapp/core/Constants.kt
@@ -3,9 +3,9 @@ package com.example.themeownetapp.core
 object Constants {
 
     //Image Type
-    const val IMAGE_TYPE_BOTH = "both"
+    const val IMAGE_TYPE_BOTH = "jpg,png,gif"
     const val IMAGE_TYPE_GIF = "gif"
-    const val IMAGE_TYPE_IMAGE = "image"
+    const val IMAGE_TYPE_IMAGE = "jpg,png"
 
     //Paging
     const val STARTING_PAGE_INDEX = 1
